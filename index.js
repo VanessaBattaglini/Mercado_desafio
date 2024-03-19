5fe632fdec1e7cfb79b9471c34f64b3f1d6e4098
Smilling Battaglini <vanessabattaglini@gmail.com>
2024-03-19 18:03:37 -0300

    :whale: Agregando ruta y gitignore

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const exphbs = require('express-handlebars');
+const PORT = 3001;
+
+//Cofiguración de platilla
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+app.engine('hbs', exphbs.engine({
+    extname: 'hbs'
+}));
+
+//Ruta static con middlewares
+app.use(express.static('assets'));
+app.use('/bootstrap', express.static(__dirname +
+    '/node_modules/bootstrap/dist'));
+
+//Rutas
+app.get('/', (req, res) => {
+    res.render('market'), {
+        products: ['banana', 'cebollas', 'pimenton', 'papas', 'lechuga', 'tomate']
+    }
+});
+
+
+app.listen(PORT, () => console.log(`El servidor se ha levantado en el port http://localhost:${PORT}`))
